7c029e4b9fe173556a941e29bcd0ef8b180f45a8
HaiNam625 <namlnh3174@ut.edu.vn>
2024-12-20 19:38:41 +0700

    Cai dat tinh nang chat bang thu vien Flask-SocketIO

--- Flask-SocketIO.py
+++ Flask-SocketIO.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template  
+from flask_socketio import SocketIO, send, emit  
 
-app = Flash(__name__)
-socketio = SocketIO(app)
+app = Flask(__name__)  
+app.config['SECRET_KEY'] = 'your_secret_key'  
+socketio = SocketIO(app)  
+
+@app.route('/')  
+def index():  
+    return render_template('index.html')  
+
+@socketio.on('message')  
+def handle_message(msg):  
+    print('Received message: ' + msg)  
+    send(msg, broadcast=True)  # Gửi tin nhắn cho tất cả người dùng  
+
+if __name__ == '__main__':  
+    socketio.run(app)
